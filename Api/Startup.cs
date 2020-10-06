f58a524daa46bcb8c72fdba5f28fd45215102d02
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-10-05 19:47:57 -0500

    20201005: Implenenting swagger

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.IdentityModel.Tokens;
 using Api.Context;
 using Api.Services;
 using AutoMapper;
+using Microsoft.OpenApi.Models;
 
 namespace Api
 {
@@ -45,7 +46,41 @@ namespace Api
                 o.JsonSerializerOptions.DictionaryKeyPolicy = null;
             });
 
+            services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "JWT Token Authentication API",
+                    Description = "ASP.NET Core 3.1 Web API"
+                });
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
 
+                    }
+                });
+            });
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
@@ -89,6 +124,7 @@ namespace Api
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+           
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -107,6 +143,16 @@ namespace Api
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
         }
     }
 }
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Install-Package Microsoft.VisualStudio.Web.CodeGeneration.Design -Version 3.1.4
 Install-Package Microsoft.EntityFrameworkCore.Tools -Version 3.1.8
 Install-Package Microsoft.EntityFrameworkCore.SqlServer -Version 3.1.8
 Install-Package Microsoft.AspNetCore.Cors
+Install-Package Swashbuckle.AspNetCore -Version 5.5.0
